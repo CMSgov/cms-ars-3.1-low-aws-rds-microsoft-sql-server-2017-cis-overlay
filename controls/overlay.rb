2d0b9767340ada50e027ac651114ab43c090d258
karikarshivani <karikarshivani@gmail.com>
2022-06-10 14:12:43 -0400

    Removed caveat for cis-1.2
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,10 +1,5 @@
 include_controls "microsoft-sql-server-2017-cis-baseline" do
 
-  control "microsoft-sql-server-2017-cis-1.2" do
-    impact 0.0
-    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not applied to this system categorization in CMS ARS 3.1"
-  end
-
   control "microsoft-sql-server-2017-cis-2.1" do
     impact 0.0
     desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not applied to this system categorization in CMS ARS 3.1"
